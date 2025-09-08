b1c7053b7bb9a52786d478b8406949a889721404
cindxyc <chencindy.1206@gmail.com>
2025-09-08 00:36:06 -0400

    changed jsx file so code is cleaner

--- app/components/polaroid.jsx
+++ app/components/polaroid.jsx
@@ -1,40 +1,30 @@
-// Basic structure - positions TBD
-// No "use client" needed - pure display component
 export default function PolaroidGallery() {
   const polaroids = [
     { id: 1,
-      frame: "/blank-polaroid.svg",
       image: "/gray-frame.svg",
       caption: "Caption for Photo 1",
       watermark: "UofTHacks 13",
-      alt: "Photo 1",
       rotation: 0,
       position: { top: 0, left: 0, zIndex: 4 },
     },
     { id: 2,
-      frame: "/blank-polaroid.svg",
       image: "/gray-frame.svg",
       caption: "Caption for Photo 2",
       watermark: "UofTHacks 13",
-      alt: "Photo 2",
       rotation: -10,
       position: { top: 20, left: -10, zIndex: 3 },
     },
     { id: 3,
-      frame: "/blank-polaroid.svg",
       image: "/gray-frame.svg",
       caption: "Caption for Photo 3",
       watermark: "UofTHacks 13",
-      alt: "Photo 3",
       rotation: 10,
       position: { top: -5, left: -5, zIndex: 2 },
     },
     { id: 4,
-      frame: "/blank-polaroid.svg",
       image: "/gray-frame.svg",
       caption: "Caption for Photo 4",
       watermark: "UofTHacks 13",
-      alt: "Photo 4",
       rotation: -2,
       position: { top: 15, left: -10, zIndex: 1 }
     }
@@ -54,7 +44,7 @@ export default function PolaroidGallery() {
           }}
         >
           {/* white polaroid frame */}
-          <img src={polaroid.frame} alt="polaroid frame" className="polaroid-frame" />
+          <img src={"/blank-polaroid.svg"} alt="polaroid frame" className="polaroid-frame" />
           {/* container for both texts and photo */}
           <div className="photo-container">
             <img src={polaroid.image} alt={polaroid.alt} className="photo" />
